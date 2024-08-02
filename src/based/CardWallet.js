afc10f8239f489e49a4faa10f0cbf5cf1f4634c6
nguyenduchoaang <duchoaang.dev@gmail.com>
2024-08-03 03:20:56 +0700

    chore: Update CardWallet component styling and structure

--- src/based/CardWallet.js
+++ src/based/CardWallet.js
@@ -21,14 +21,32 @@ export const CardWallet = (props) => {
           <div className="info_address">
             <h4>
               <span>Address</span>
-              <ButtonAction props={{ width: "34px", height: "34px" }}>
-                <Copy width="20" height="20" />
+              <ButtonAction
+                props={{
+                  width: "24px",
+                  height: "24px",
+                  justify: "center",
+                  align: "center",
+                }}
+              >
+                <Copy width="16" height="16" />
               </ButtonAction>
             </h4>
             <p>{address}</p>
           </div>
         </div>
-        <div className="card_wallet_body"></div>
+        <div className="card_wallet_body">
+          <div className="body_top">
+            <h4>BALANCE</h4>
+            <span>
+              <div className="dot"></div>
+            </span>
+            <p>MTD</p>
+          </div>
+          <div className="body_bottom">
+            <p>{balance} wei</p>
+          </div>
+        </div>
       </CardInner>
     </CardWrapper>
   );
@@ -38,6 +56,7 @@ const CardWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  font-family: "Inter", sans-serif;
 `;
 
 const CardInner = styled.div`
@@ -50,14 +69,21 @@ const CardInner = styled.div`
   border-radius: 36px;
   padding: 24px 32px;
   background: ${(props) => props.bgColor};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  font-family: "Inter", sans-serif;
+
   .card_wallet_header {
     display: flex;
+    font-family: "Inter", sans-serif;
     justify-content: space-between;
     width: 100%;
     margin-top: 24px;
     flex-direction: column;
     .name_left {
       h4 {
+        font-family: "Inter", sans-serif;
         font-size: 15px;
         font-weight: 500;
         line-height: 14.52px;
@@ -66,6 +92,7 @@ const CardInner = styled.div`
         margin-bottom: 8px;
       }
       p {
+        font-family: "Inter", sans-serif;
         font-size: 22px;
         font-weight: 700;
         line-height: 20px;
@@ -74,25 +101,86 @@ const CardInner = styled.div`
       }
     }
     .name_right {
+      font-family: "Inter", sans-serif;
       display: flex;
       justify-content: center;
       align-items: center;
     }
     .info_name {
       width: 100%;
+      font-family: "Inter", sans-serif;
       display: flex;
       justify-content: space-between;
     }
     .info_address {
+      font-family: "Inter", sans-serif;
       margin-top: 21px;
       h4 {
         margin-bottom: 12px;
-
+        font-family: "Inter", sans-serif;
         span {
+          font-family: "Inter", sans-serif;
           margin-right: 8px;
         }
         display: flex;
         align-items: center;
+      }
+      p {
+        font-family: "Inter", sans-serif;
+        font-size: 20px;
+        font-weight: 400;
+        line-height: 26px;
+        text-align: left;
+      }
+    }
+  }
+  .card_wallet_body {
+    background: rgba(245, 245, 245, 0.12);
+    border-radius: 16px;
+    padding: 13px 16px;
+    display: flex;
+    flex-direction: column;
+    .body_top {
+      display: flex;
+      align-items: center;
+      h4 {
+        font-family: "Inter", sans-serif;
+        font-size: 12px;
+        font-weight: 400;
+        line-height: 14.52px;
+        letter-spacing: -0.02em;
+        text-align: left;
+      }
+      p {
+        font-family: "Inter", sans-serif;
+        font-size: 12px;
+        font-weight: 700;
+        line-height: 14.52px;
+        letter-spacing: -0.02em;
+        text-align: left;
+      }
+      span {
+        margin: 0 8px;
+        font-size: 12px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        .dot {
+          width: 4px;
+          height: 4px;
+          border-radius: 50%;
+          background: #fff;
+        }
+      }
+    }
+    .body_bottom {
+      p {
+        font-family: "Inter", sans-serif;
+        font-size: 20px;
+        font-weight: 700;
+        line-height: 22px;
+        letter-spacing: -0.02em;
+        text-align: left;
       }
     }
   }
